2fc54822a89885204cce5b091595ce2783fca266
Eric Claus <ericclaus@nadadventist.org>
2020-09-29 17:42:39 -0400

    readded files due to incorrect line endings

---
+++ AutoLoad/Start-RickRoll.ps1
@@ -0,0 +1,11 @@
+﻿function Start-RickRoll {
+    <#
+    .SYNOPSIS
+        Rick Roll in PowerShell
+
+    .LINK
+        https://www.nextofwindows.com/powershell-fun-watch-rick-astley-sing-and-dance-never-let-you-down
+    #>
+
+    iex (New-Object Net.WebClient).DownloadString("http://bit.ly/e0Mw9w")
+}
